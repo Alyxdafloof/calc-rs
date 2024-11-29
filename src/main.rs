9800d1df10466ee2b4b47297e7e5810065db1173
alyxdafloof <Auroraholtman@gmail.com>
2024-11-28 20:07:36 -0600

    Feature: adds program run loop and user based break functionality

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+//Calculator program for Rust//
+
+//Libraries used//
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+
+    loop {
+        println!("What operation are you using?");
+        println!("1.) Addition");
+        println!("2.) Subtraction");
+        println!("3.) Multiplication");
+        println!("4.) Division");
+        println!("5.) Exit");
+
+        let mut op = String::new();
+        io::stdin()
+            .read_line(&mut op)
+            .expect("couldnt read line");
+        let op = op.trim().parse().expect("Could not parse");
+
+        match op {
+            5 => break,
+            _ => println!("Please input one of the above numbers"),
+        }
+    }
 }
