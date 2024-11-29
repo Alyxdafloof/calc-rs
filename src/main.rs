5897de119b1c1bfa1b29e3366e9316d0049d71fb
alyxdafloof <Auroraholtman@gmail.com>
2024-11-28 20:54:52 -0600

    Added user input for eatch operator function, temprorary solution fix later

--- src/main.rs
+++ src/main.rs
@@ -20,8 +20,76 @@ fn main() {
         let op = op.trim().parse().expect("Could not parse");
 
         match op {
+            1 => addition(),
+            2 => subtraction(),
+            3 => multiplication(),
+            4 => division(),
             5 => break,
             _ => println!("Please input one of the above numbers"),
         }
     }
+}
+
+fn addition () {
+    println!("What is your first number(to the first decimal)");
+    let mut x = String::new();
+    io::stdin()
+        .read_line(&mut x)
+        .expect("couldnt read line");
+    let x: f64 = x.trim().parse().expect("cant parse");
+
+    println!("What is your second number?(to the first decimal)");
+    let mut y = String::new();
+    io::stdin()
+        .read_line(&mut y)
+        .expect("couldnt read line");
+    let y: f64 = y.trim().parse().expect("cant parse");
+}
+
+fn subtraction () {
+    println!("what is your first number(to the first decimal)");
+let mut x = String::new();
+io::stdin()
+	.read_line(&mut x)
+	.expect("couldnt read line");
+let x: f64 = x.trim().parse().expect("couldnt parse");
+
+println!("what is your second number(to the first decimal)");
+let mut y = String::new();
+io::stdin()
+	.read_line(&mut y)
+	.expect("couldnt read line");
+let y: f64 = y.trim().parse().expect("couldnt parse");
+}
+
+fn multiplication () {
+    println!("what is your first number(to the first decimal)");
+let mut x = String::new();
+io::stdin()
+	.read_line(&mut x)
+	.expect("couldnt read line");
+let x: f64 = x.trim().parse().expect("couldnt parse");
+
+println!("what is your second number(to the first decimal)");
+let mut y = String::new();
+io::stdin()
+	.read_line(&mut y)
+	.expect("couldnt read line");
+let y: f64 = y.trim().parse().expect("couldnt parse");
+}
+
+fn division () {
+    println!("what is your first number(to the first decimal)");
+let mut x = String::new();
+io::stdin()
+	.read_line(&mut x)
+	.expect("couldnt read line");
+let x: f64 = x.trim().parse().expect("couldnt parse");
+
+println!("what is your second number(to the first decimal)");
+let mut y = String::new();
+io::stdin()
+	.read_line(&mut y)
+	.expect("couldnt read line");
+let y: f64 = y.trim().parse().expect("couldnt parse");
 }
